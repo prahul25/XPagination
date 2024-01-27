d828e7ae28a5346d615e51dcce60235d93fec017
prahul25 <palrahulkumar2002@gmail.com>
2024-01-28 02:11:06 +0530

    Project completed

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  // Fetch data from API
+  useEffect(() => {
+    fetch(
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setTotalPages(Math.ceil(data.length / 10));
+      })
+      .catch((err) => alert("failed to fetch data"));
+  }, []);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const handleNextClick = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  const preDisabled = currentPage === 1;
+  const nextDisabled = currentPage === totalPages;
+
+  const itemsPerPage = 10;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const itemsToDisplay = data.slice(startIndex, endIndex);
+
+  return (
+    <div className="App">
+      <h1>Employee Data Table</h1>
+      <table>
+        <thead>
+          <tr  style={{display:"flex" , width:"100%", justifyContent:"space-between"}}>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {itemsToDisplay.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.name}</td>
+              <td>{employee.email}</td>
+              <td>{employee.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="pagination">
+        <button onClick={handlePrevClick} disabled={preDisabled}>
+          Prev
+        </button>
+        <span>{currentPage}</span>
+        <button onClick={handleNextClick} disabled={nextDisabled}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
